b6cd60db6c0640949ac9a1b368b5c2b9a00b45c2
LeonID <lmatskevich79@gmail.com>
2022-11-07 14:08:17 +0300

    feat: task 04-copy-directory completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDir() {
+  await fs.promises.rm(path.relative(__dirname, 'files-copy'), {
+    recursive: true,
+    force: true,
+  });
+
+  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), {
+    recursive: true,
+  });
+
+  await fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.resolve(__dirname, 'files', file),
+        path.resolve(__dirname, 'files-copy', file),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    });
+  });
+}
+copyDir();
