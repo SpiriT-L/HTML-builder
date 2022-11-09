80cd64e62ada9771824d8da1c737e55bcf2539ac
LeonID <lmatskevich79@gmail.com>
2022-11-10 00:18:17 +0300

    feat: task 04-copy-directory completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
-async function copyDir() {
-  await fs.promises.rm(path.relative(__dirname, 'files-copy'), {
-    recursive: true,
-    force: true,
-  });
+fs.rm(
+  path.resolve(__dirname, 'files-copy'),
+  { recursive: true, force: true },
+  (errorRm) => {
+    if (errorRm) throw new Error(errorRm);
 
-  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), {
-    recursive: true,
-  });
+    fs.mkdir(
+      path.resolve(__dirname, 'files-copy'),
+      { recursive: true },
+      (errorMkdir) => {
+        if (errorMkdir) throw new Error(errorMkdir);
+      }
+    );
 
-  await fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
-    files.forEach((file) => {
-      fs.copyFile(
-        path.resolve(__dirname, 'files', file),
-        path.resolve(__dirname, 'files-copy', file),
-        (err) => {
-          if (err) throw err;
-        }
-      );
-    });
-  });
-}
-copyDir();
+    fs.readdir(
+      path.resolve(__dirname, 'files'),
+      { withFileTypes: true },
+      (errorRaeddir, files) => {
+        if (errorRaeddir) throw new Error(errorRaeddir);
+        files.forEach((file) => {
+          if (file.isFile()) {
+            let oldDirectory = path.resolve(__dirname, 'files', file.name);
+            let copyDirectory = path.resolve(
+              __dirname,
+              'files-copy',
+              file.name
+            );
+            fs.copyFile(oldDirectory, copyDirectory, (errorCopyFile) => {
+              if (errorCopyFile) throw new Error(errorCopyFile);
+            });
+          }
+        });
+      }
+    );
+  }
+);
